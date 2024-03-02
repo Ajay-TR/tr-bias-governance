5d46cc7f5a525416eadfa9b7f5abf624ea76c1c8
Shourya Jain <shourya242@gmail.com>
2024-03-02 11:05:01 +0530

    Anova and Demographic Parity

--- main.py
+++ main.py
@@ -22,3 +22,13 @@ def chi2_test(observed, alpha=0.05):
     chi2, p = chisquare(observed, f_exp=expected)
     return p < alpha
 
+def anova_bias(col1, col2):
+    res = f_oneway(col1.tolist(),col2.tolist())
+    print("H0 Accepted") if res.pvalue > 0.05 else print("H0 Rejected")
+    
+def demographic_parity(col1, col2,threshold):
+    ratio1 = sum(col1) / len(col1)
+    ratio2 = sum(col2) / len(col2)
+    print("Bias exists") if(abs(ratio1-ratio2) > threshold) else print("No bias")
+
+
